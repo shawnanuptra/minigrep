aecebf4007a495ab342469d11b02d4f5aa243ed1
Shawn <shawn.martin@sunderlandsoftwarecity.com>
2024-09-27 21:17:46 +0100

    Created Config struct, and its impl. removed parse_config(), made into new() in Config impl

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,29 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // args will print out ['binary path', other arguments...]
 
-    // because args[0] will print out binary path, we need to take args[0] and args[1] for query, filename
-    let query = &args[1];
-    let filename = &args[2];
+    let config: Config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
     // fs (filesystem) reads content file, return as Result<Ok, Err>, where Ok is a String
-    let contents = fs::read_to_string(filename).expect("File can't be found");
+    let contents = fs::read_to_string(config.filename).expect("File can't be found");
 
     println!("With text:\n {}", contents)
+}
+
+struct Config<'a> {
+    query: &'a str,
+    filename: &'a str,
+}
+
+impl<'a> Config<'a> {
+    fn new(args: &[String]) -> Config {
+        // because args[0] will print out binary path, we need to take args[0] and args[1] for query, filename
+        let query = &args[1];
+        let filename = &args[2];
+
+        // return Config instance
+        Config { query, filename }
+    }
 }
