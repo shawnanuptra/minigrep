d236fcd916469f404eddd3c5ae60c37d40f3db75
Shawn <shawn.martin@sunderlandsoftwarecity.com>
2024-09-28 13:49:21 +0100

    added test and added search() functionality

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@ use std::{error::Error, fs};
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // fs (filesystem) reads content file, return as Result<Ok, Err>, where Ok is a String
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n {}", contents);
+
+    // loop over the Vec<&str> from search(), and print to console
+    for line in search(&config.query, &contents) {
+        println!("{}", line)
+    }
 
     Ok(())
 }
@@ -25,5 +29,32 @@ impl<'a> Config<'a> {
 
         // return Config instance
         Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
     }
 }
